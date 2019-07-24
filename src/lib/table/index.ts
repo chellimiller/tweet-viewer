3647544738da9e0410235d4641e9c52908d3eb67
Michelle <chellicmiller@gmail.com>
2019-07-23 22:51:10 -0500

    Add the table, column, and row interfaces

--- src/lib/table/index.ts
+++ src/lib/table/index.ts
@@ -1,1 +1,19 @@
+import { Tweet, TwitterApi } from '../twitter-api-wrapper';
+
 export * from './table.component';
+
+export interface Column<T> {
+    id: string;
+    property: keyof T;
+    displayName: string;
+}
+
+export interface Row<T> {
+    data: T;
+}
+
+export interface Table<T> {
+    componentId: string;
+    columns: Column<T>[];
+    rows: Row<T>[];
+}
